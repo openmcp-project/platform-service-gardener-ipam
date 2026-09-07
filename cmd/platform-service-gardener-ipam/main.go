860c1f12cdcdb8f6d499ddf3bc2d1d3ed5b14b85
René Schünemann <rene.schuenemann@sap.com>
2026-09-07 15:42:27 +0200

    chore: enable FIPS 140-3 verification (#41)
    * chore: add fips verification
    
    On-behalf-of: @SAP rene.schuenemann@sap.com
    Signed-off-by: René Schünemann (SAP) <rene.schuenemann@sap.com>
    
    * feat: release v0.4.0
    
    On-behalf-of: @SAP rene.schuenemann@sap.com
    Signed-off-by: René Schünemann (SAP) <rene.schuenemann@sap.com>
    
    ---------
    
    Signed-off-by: René Schünemann (SAP) <rene.schuenemann@sap.com>

--- cmd/platform-service-gardener-ipam/main.go
+++ cmd/platform-service-gardener-ipam/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/openmcp-project/platform-service-gardener-ipam/cmd/platform-service-gardener-ipam/app"
+
+	"github.com/openmcp-project/controller-utils/pkg/fips"
 )
 
 func main() {
+	fips.Verify(context.Background())
+
 	cmd := app.NewPlatformServiceGardenerIPAMCommand()
 
 	if err := cmd.Execute(); err != nil {
